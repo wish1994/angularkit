6ed395c6fd65c4e127762cf4cf1467e6d0ba67e3
wishchan <253094267@qq.com>
2017-12-13 09:30:08 +0800

    提交

---
+++ build/static/js/all.js
@@ -0,0 +1,65 @@
+var app = angular.module('starkapp',[
+  'ngRoute',
+  'controllers',
+  'services'
+])
+
+angular.module('controllers',[]);
+angular.module('services',[]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+      template: '<h1>今天好冷哦！{{msg}}</h1>',
+      controller: function ($scope) {
+        $scope.msg = "是捏，就是好冷！";
+      }
+    })
+      .when('/stark', {
+        template: '<h1>this is stark page</h1>',
+        controller: function ($scope) {
+          $scope.msg = "是捏，就是好冷！";
+        }
+      })
+      .when('/starkwang', {
+        template: '<h1>this is starkwang page</h1>',
+        controller: function ($scope) {
+          $scope.msg = "是捏，嘿嘿！";
+        }
+      })
+      .when('/shudong', {
+        templateUrl: 'tpl.html',
+        controller: function ($scope) {
+          $scope.msg = "是捏，就是好冷！";
+        }
+      })
+      .when('/goodslist', {
+        templateUrl: 'view/goodsList.html',
+        controller: 'GoodsController'
+      })
+
+    // 需要在localhost下面运行
+  })
+angular.module('controllers').controller('GoodsController',[
+  '$scope',
+  '$route',
+  '$routeParams',
+  'GoodsService',
+  function(
+    $scope,
+    $route,
+    $routeParams,
+    GoodsService
+  ){
+    $scope.goodsList = GoodsService.fetchGoodsList();
+  }])
+angular.module('starkapp')
+    .factory('GoodsService',['$http',function($http) {
+      return{
+        fetchGoodsList:function(){
+          return $http.get('https://easy-mock.com/mock/59664d4d58618039284c7710/example/goods/list').then(function(data){
+            return data;
+          })
+        }
+      }
+  
+}])
